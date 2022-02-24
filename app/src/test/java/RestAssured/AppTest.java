fe1b57e643a311cf09aa7d5f75c9138875531c31
Diogo <oxediogo25@gmail.com>
2022-02-24 18:57:20 -0300

    add basePath

--- app/src/test/java/RestAssured/AppTest.java
+++ app/src/test/java/RestAssured/AppTest.java
@@ -9,8 +9,7 @@ import org.apache.http.HttpStatus;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
@@ -18,12 +17,17 @@ import static org.junit.Assert.*;
 public class AppTest {
     @BeforeClass
     public static void setup(){
+        //Mostrar detalhes quando o test falhar
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        //Caminhos base para acesso a API
+        baseURI = "https://reqres.in";
+        basePath = "/api";
     }
     @Test
     public void testDadosDoUsuario() {
+        given().params("page", "2").
         when().
-                get("https://reqres.in/api/users?page=2").
+                get("/users").
         then().
                 statusCode(HttpStatus.SC_OK).
                 body("page", is(2)).
@@ -35,7 +39,7 @@ public class AppTest {
                 contentType(ContentType.JSON).
                 body("{\"name\": \"diogo\",\"job\": \"leader\"}").
         when().
-                post("https://reqres.in/api/users").
+                post("/users").
         then().
                 statusCode(HttpStatus.SC_CREATED).body("name", is("diogo"));
     }
